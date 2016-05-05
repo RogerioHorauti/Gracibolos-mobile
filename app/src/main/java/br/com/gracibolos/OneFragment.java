8359834a97f98c7ad3dd614e847932f6de844b69
RogerioHorauti <rogeriohorauti@gmail.com>
2016-05-05 12:15:28 -0300

    Os fragmentos On e Two finalizados

--- app/src/main/java/br/com/gracibolos/OneFragment.java
+++ app/src/main/java/br/com/gracibolos/OneFragment.java
@@ -3,12 +3,20 @@ package br.com.gracibolos;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class OneFragment extends Fragment {
+
+    ImageView imageView;
+    TextView textViewObs;
+    TextView textViewPreco;
+    TextView textViewCodigo;
 
     public OneFragment() {
     }
@@ -16,8 +24,6 @@ public class OneFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
     }
 
     @Override
@@ -26,11 +32,23 @@ public class OneFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.activity_one_fragment, container, false);
 
+        imageView = (ImageView) view.findViewById(R.id.imageViewProduto);
+        textViewObs = (TextView) view.findViewById(R.id.textViewObs);
+        textViewCodigo = (TextView) view.findViewById(R.id.textViewCodigo);
+        textViewPreco = (TextView) view.findViewById(R.id.textViewPreco);
+
         Bundle params = this.getArguments();
         //Bundle não nulo
         if (params != null) {
             Produto p = (Produto) params.getSerializable("produto");
-            Log.i("rest", p.getNome());
+
+            Picasso.with(getContext())
+                    .load(new Url().getUrl() + "/resources/img/produtos/" + p.getFoto())
+                    .into(imageView);
+
+            textViewObs.setText(p.getObs());
+            textViewCodigo.setText(p.getCodigo());
+            textViewPreco.setText(String.valueOf(p.getValor()));
         }
 
         return view;
--- app/src/main/java/br/com/gracibolos/TwoFragment.java
+++ app/src/main/java/br/com/gracibolos/TwoFragment.java
@@ -3,12 +3,18 @@ package br.com.gracibolos;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class TwoFragment extends Fragment {
+
+    private ImageView imageView;
+    private TextView textViewReceita;
 
     public TwoFragment() {
     }
@@ -24,11 +30,19 @@ public class TwoFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.activity_two_fragment, container, false);
 
+        imageView = (ImageView) view.findViewById(R.id.imageViewReceita);
+        textViewReceita = (TextView) view.findViewById(R.id.textViewTeceita);
+
         Bundle params = this.getArguments();
         //Bundle não nulo
         if (params != null) {
             Produto p = (Produto) params.getSerializable("produto");
-            Log.i("rest", p.getReceita());
+
+            Picasso.with(getContext())
+                    .load(new Url().getUrl() + "/resources/img/produtos/capa2.png")
+                    .into(imageView);
+
+            textViewReceita.setText(p.getReceita());
         }
 
         return view;
