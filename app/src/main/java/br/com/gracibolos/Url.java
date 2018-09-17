a477f6c4607b9667c19b8eb44dcf906e15f09d5d
Rogerio Yudi Horauti <rogeriohorauti@gmail.com>
2018-09-16 21:04:31 -0300

    replace name receita to rec

--- app/src/main/java/br/com/gracibolos/Url.java
+++ app/src/main/java/br/com/gracibolos/Url.java
@@ -9,9 +9,9 @@ public class Url
 
     public String getUrl()
     {
-        //this.url = "http://192.168.46.19:8080/Gracibolos";
+        this.url = "http://191.188.182.108:8080/Gracibolos";
         //this.url = "http://172.20.22.219:8080/Gracibolos";
-        this.url = "http://192.168.1.107:8080/Gracibolos";
+        //this.url = "http://192.168.1.109:8080/Gracibolos";
         //this.url = "http://172.20.25.19:8080/Gracibolos";
 
         return this.url;
